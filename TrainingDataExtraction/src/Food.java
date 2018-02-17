52416a89d423862c7791f65b8c1f204923b01146
Xiang Li <xli150@jhu.edu>
2018-02-17 14:59:35 -0500

    lisa
    lisa

--- TrainingDataExtraction/src/Food.java
+++ TrainingDataExtraction/src/Food.java
@@ -1,26 +1,51 @@
+import util.HTTPGetter;
+import util.MyParser;
 
 public class Food {
     private String rawName; // The name recognized from the menu.
     private String name; // The official name of this dish.
+    private String cookname;
     private String description; // Detailed description of the dish got from google knowledge graph.
     private double[] ingredientRawVector;
     private double[] subcataRawVector;
     private double[] cataRawVector;
 
+    public Food (String rawName) {
+        this.rawName = rawName;
+    }
     public String getName() {
-        return name;
+        String splited =  this.rawName.replaceAll("\\s", "+");
+        String URL = "https://www.bing.com/search?q=" + splited;
+        System.out.println(splited);
+        HTTPGetter getOfficial = new HTTPGetter(URL);
+        String html1 = getOfficial.getHTML();
+        MyParser aa = new MyParser();
+        String realname = aa.BingHTMLParser(html1);
+        this.name = realname;
+        return this.name;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public String getDescription() {
-        return description;
+    public String getTurName() {
+        String splited =  this.rawName.replaceAll("\\s", "+");
+        String URL = "https://en.wikibooks.org/w/index.php?search=" + splited;
+        HTTPGetter getOfficial = new HTTPGetter(URL);
+        String html1 = getOfficial.getHTML();
+        MyParser aa = new MyParser();
+        String bb = aa.CookbookSearchHTMLParser(html1);
+        this.cookname = bb;
+        return bb;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public String setDescription() {
+        //https://en.wikibooks.org/wiki/Cookbook:Mapo_Doufu
+        String URL = "https://en.wikibooks.org/wiki/Cookbook:" + this.cookname;
+        HTTPGetter getOfficial = new HTTPGetter(URL);
+        String html1 = getOfficial.getHTML();
+        return html1;
     }
 
     public String getRawName() {
@@ -53,6 +78,17 @@ public class Food {
 
     public void setCataRawVector(double[] cataRawVector) {
         this.cataRawVector = cataRawVector;
+    }
+
+    static public void main(String[] args) {
+        Food obj = new Food("Mapo tofu");
+        String realName = obj.getName();
+        System.out.println (realName);
+        String aa = obj.getTurName();
+        System.out.println (aa);
+        String outt = obj.setDescription();
+        System.out.println (outt);
+
     }
 
 }
