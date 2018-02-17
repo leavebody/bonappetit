b170f6a226de80db17495e1e21a9159ec747aa50
Xiaochen Li <leave.body@hotmail.com>
2018-02-17 10:01:39 -0500

    training data framework

--- TrainingDataExtraction/src/util/MyParser.java
+++ TrainingDataExtraction/src/util/MyParser.java
@@ -1,6 +1,8 @@
 package util;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MyParser {
     /**
@@ -11,28 +13,29 @@ public class MyParser {
      * @return a string of name or null
      */
     public static String BingHTMLParser(String html){
-        //todo
-    	int index = html.indexOf("</strong> - <strong>Wikipedia</strong>}");
-    	String temp = null;
-    	int start = index - 1
-    	int end = start + 9
-		while(html.substring(start,end)!="<strong>" && start > 0){
-			temp = html.substring(start,end);
-			start--;
-			end--;
-		}
-
-
-        String regexe = "/\\<strong>\\w+\\<strong> - <strong>Wikipedia</strong>}/";
-        Pattern pattern = Pattern.compile(regexe);
-		Matcher matcher = pattern.matcher(html);
-		if (matcher.find()) {
-			String result = matcher.group(0);
-			length = length(result)
-    		return result.substring(8, length-36 ) //prints /{item}/
-		} else {
-    		return null;
-		}
+//        //todo
+//    	int index = html.indexOf("</strong> - <strong>Wikipedia</strong>}");
+//    	String temp = null;
+//    	int start = index - 1
+//    	int end = start + 9
+//		while(html.substring(start,end)!="<strong>" && start > 0){
+//			temp = html.substring(start,end);
+//			start--;
+//			end--;
+//		}
+//
+//
+//        String regexe = "/\\<strong>\\w+\\<strong> - <strong>Wikipedia</strong>}/";
+//        Pattern pattern = Pattern.compile(regexe);
+//		Matcher matcher = pattern.matcher(html);
+//		if (matcher.find()) {
+//			String result = matcher.group(0);
+//			length = length(result);
+//    		return result.substring(8, length-36 ); //prints /{item}/
+//		} else {
+//    		return null;
+//		}
+        return null;
     }
 
     /**
@@ -43,5 +46,18 @@ public class MyParser {
     public static ArrayList<String> CookbookHTMLParser(String html){
         //todo
         return new ArrayList<String>();
+    }
+
+    /**
+     * Get the cookbook name of the dish from html string of the cookbook search web page.
+     * Find the first "/wiki/Cookbook:" in the string, and extract the string between it and the next "\"".
+     * If there is no "/wiki/Cookbook:", abort.
+     * If the extracted string equals "Table_of_Contents", abort.
+     * @param html raw html code of the web page
+     * @return the cookbook name, eg. Mapo_Doufu. Or null if aborted.
+     */
+    public static String CookbookSearchHTMLParser(String html){
+        //todo
+        return new String();
     }
 }
